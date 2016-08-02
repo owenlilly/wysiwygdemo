737201dd3c9f85ddd81837dbd0fab60034cfbd72
Owen Lilly <owelilly@gmail.com>
2016-08-02 12:38:55 -0500

    Added basic angular material setup

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,1 +1,13 @@
-var mainApp = angular.module('mainApp', []);
+const mainApp = angular.module('mainApp', ['ngMaterial', 'ngMdIcons'])
+                        .config(function($mdIconProvider){
+                            $mdIconProvider
+                                .icon('menu', '/img/icons/menu.svg')
+                                .icon('more_vert', '/img/icons/more_vert.svg')
+                                .icon('search', '/img/icons/search.svg')
+                                .icon('favorite', '/img/icons/favorite.svg');
+                        })
+                        .config(function($mdThemingProvider){
+                            $mdThemingProvider.theme('default')
+                                                .primaryPalette('blue')
+                                                .accentPalette('yellow');
+                        });
