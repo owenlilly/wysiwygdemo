d4a6d25c58fca5442c73be954834b19ce7ba682b
Owen Lilly <owelilly@gmail.com>
2016-08-03 03:51:02 -0500

    Added session and rxmongo connect

--- server.js
+++ server.js
@@ -5,7 +5,13 @@ const express    = require('express'),
 	  ejs        = require('ejs'),
 	  app        = express(),
 	  bodyParser = require('body-parser'),
-	  homeController = require('src/controllers/home/HomeController');
+      session    = require('express-session'),
+      RxMongo    = require('rxmongo'),
+	  homeController = require('src/controllers/home/HomeController'),
+	  accountController = require('src/controllers/account/AccountController'),
+	  articleController = require('src/controllers/home/ArticleController');
+
+const mongoUrl = 'mongodb://localhost/findit';
 
 // setup view engine
 app.set('views', `${__dirname}/src/views`);
@@ -14,10 +20,19 @@ app.engine('html', engine);
 
 // setup middlewares
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(`${__dirname}/src/public`));
+app.use(session({
+  secret: '9D59516154098C8BFFCABDFB261646BC210F37F77D4A3D8C23654FC480DDCD88',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
 
 // controllers
 app.use('/', homeController);
+app.use('/account', accountController);
+app.use('/u', articleController);
 
 // error handler
 app.use((err, req, res, next) => {
@@ -29,6 +44,9 @@ app.use((err, req, res, next) => {
 
 // start!
 const port = process.env.PORT || 3000;
-app.listen(port, function(){
-    console.log(`running on port ${port}`)
-})
+RxMongo.connect(mongoUrl)
+        .subscribe(db => {
+                            app.listen(port, function(){
+                                console.log(`running on port ${port}`)
+                            });
+                    });
